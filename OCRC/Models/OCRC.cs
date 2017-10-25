f4f0dcdf761f4b5e2efc69fef4ad4d2ae1ab92b8
Tim3Ds@gmail.com <Tim3Ds@gmail.com>
2017-10-25 11:51:43 -0600

    Add project files.

---
+++ OCRC/Models/OCRC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+
+namespace OCRC.Models
+{
+    public class OCRC
+    {
+        //TODO:create CRUD methods
+    }
+
+
+
+    //TODO: create models
+
+    public class OCRCDbContext : DbContext
+    {
+
+        //TODO: create DbSets
+    }
+}
---
+++ OCRC/Models/Repo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OCRC.Models
+{
+    public class Repo
+    {
+        // Tim
+
+
+        // Nas
+
+
+        // yi
+
+
+        // Heather
+
+
+        // Eric
+
+
+        // Mohammed
+
+
+        // Hoang
+
+
+
+    }
+}
